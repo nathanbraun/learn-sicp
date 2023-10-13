a1713db902d7638ce8d79b21aca68b6306d6b5e1
Nathan Braun <nathanericbraun@gmail.com>
2023-10-13 11:11:03 -0500

    Move code to new file

---
+++ sicp-1-1-7.rkt
@@ -0,0 +1,48 @@
+#|
+
+takes a guess and number we want to find square root of if "good enough" returns
+guess otherwise runs the function again with a better guess (improve guess x)
+
+requires defining "good enough" and "improve"
+
+improve :: average guess with x/guess
+good enough :: x/guess within +- 0.00001 of guess
+
+|#
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x))) 
+
+(define (improve guess x)
+  (average guess (/ x guess))) 
+
+(define (average x y)
+  (/ (+ x y) 2)) 
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.00001)) 
+
+(define (square x)
+  (* x x))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x)) 
+
+#|
+flexible version - for trying out different thresholds
+|#
+(define (sqrt-iter-flex guess x threshold)
+  (if (good-enough-flex? threshold guess x)
+    guess
+    (sqrt-iter-flex (improve guess x) x threshold))) 
+
+(define (good-enough-flex? threshold guess x)
+  (< (abs (- (square guess) x)) threshold)) 
+
+(define (sqrt-flex x threshold)
+  (let ([guess 1.0]) (sqrt-iter-flex guess x threshold))
+  ) 
+
+
